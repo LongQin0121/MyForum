e9f16f99d3a9706f56ac19d710e2841895084bc7
Long Qin <qinlong0121@gmail.com>
2024-01-28 16:41:54 +0800

    added timer

--- static/js/register.js
+++ static/js/register.js
@@ -1,19 +1,33 @@
 //alert("register.js")
 
-//整个网页都加载完毕后，再执行的的
-$(function(){
+function bindEmailCaptchaClick(){
     $("#captcha-btn").click(function(event){
+        //$this means current buttom and object of jquery 
+        var $this = $(this);
+        //prevent default event
         event.preventDefault();
 
         var email = $("input[name='email']").val();
-
+        console.log("Email:", email); // Add this line to log the email value
         $.ajax({
             url: "captcha/email?email=" + email,
             method: "GET",
             success: function(result) {
                 var code = result['code'];
                 if(code == 200){
-                    alert("Validation Code sending Success");
+                    var countdown = 5;
+                    //开始倒计时之前，就取消按钮的点击事件
+                    $this.off("click");
+                    var timer = setInterval(function (){
+                        $this.text(countdown);
+                        countdown -= 1;
+                        if(countdown <= 0){
+                            clearInterval(timer);
+                            $this.text("获取验证码");
+                            bindEmailCaptchaClick();
+                        }
+                    }, 1000);
+                    //alert("Email verification code sent successfully.");
                 } else {
                     alert(result['message']);
                 }
@@ -23,4 +37,10 @@ $(function(){
             }
         })
     });
+}
+
+
+//整个网页都加载完毕后，再执行的的
+$(function(){
+    bindEmailCaptchaClick();
 });
